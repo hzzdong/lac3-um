a767b7aa97abab063c74fff8caa06ff138cdc9d8
kaige888 <279259918@qq.com>
2020-07-28 11:46:44 +0800

    dubbo 标签修改

--- lac3-um-web-oapi/src/main/java/com/linkallcloud/um/web/oapi/face/holiday/HolidayFace.java
+++ lac3-um-web-oapi/src/main/java/com/linkallcloud/um/web/oapi/face/holiday/HolidayFace.java
@@ -2,7 +2,7 @@ package com.linkallcloud.um.web.oapi.face.holiday;
 
 import java.util.Calendar;
 
-import org.apache.dubbo.config.annotation.Reference;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,7 +20,7 @@ import com.linkallcloud.web.face.annotation.Face;
 @Module(name = "假日")
 public class HolidayFace {
 
-	@Reference(version = "${dubbo.service.version}", application = "${dubbo.application.id}")
+	@DubboReference(version = "${dubbo.service.version}", application = "${dubbo.application.id}")
 	private IHolidayDateManager holidayDateManager;
 
 	@Face(login = false)
